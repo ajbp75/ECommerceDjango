e45c04d32d0412a8bfe6956cd8e1507bae6a1c91
Amadeu Paes <amadeupaes75@gmail.com>
2022-06-18 14:52:00 -0300

    finalizado user

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import RegisterForm
 
@@ -16,3 +17,7 @@ def register(request):
         form = RegisterForm()
 
     return render(request, 'users/register.html',{'form':form})
+
+@login_required
+def profilepage(request):
+    return render(request,'users/profile.html')
