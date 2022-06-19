1400d01e7a446bf087d50432dccd7c8a99cfa0f1
Amadeu Paes <amadeupaes75@gmail.com>
2022-06-19 17:37:05 -0300

    Project  1 penultima part

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -27,10 +26,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'movies.apps.MoviesConfig',
     'users.apps.UsersConfig',
     'food.apps.FoodConfig',
     'django.contrib.admin',
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -81,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -101,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -113,16 +110,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL='food:index'
+LOGIN_REDIRECT_URL = 'food:index'
 LOGIN_URL = 'login'
-MEDIA_ROOT= BASE_DIR / 'pictures'
-MEDIA_URL='/pictures/'
-
+MEDIA_ROOT = BASE_DIR / 'pictures'
+MEDIA_ROOT_MOVIE = BASE_DIR / 'media'
+MEDIA_URL = '/pictures/'
+MEDIA_URL_MOVIE = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,25 +13,36 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import include, path
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from movies.views import MovieViewSet, ActionViewSet, ComedyViewSet
 
+router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('action', ActionViewSet)
+router.register('comedia', ComedyViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
-    path('register/', user_views.register,name='register'),
-    path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/',user_views.profilepage,name='profile'),
+    path('register/', user_views.register, name='register'),
+    path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profilepage, name='profile'),
+    path('', include(router.urls)),
 ]
 
-#Para incluir as imagens, quando chamar 
+# Para incluir as imagens, quando chamar
 urlpatterns += [
-    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Para incluir as imagens, quando chamar (movie)
+urlpatterns += [
+
+               ] + static(settings.MEDIA_URL_MOVIE, document_root=settings.MEDIA_ROOT_MOVIE)
